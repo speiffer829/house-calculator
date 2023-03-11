818e63dd973ee5a9a98694fd9c62121af4075fa2
Spenser <spenser@minddnd.com>
2023-03-11 01:29:14 -0500

    stats mobile styles

--- src/lib/Stats.svelte
+++ src/lib/Stats.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { ArrowUp, ArrowDown } from 'lucide-svelte';
+
 	export let data;
 
 	$: latest = data.observations.at(-1);
-	$: get_trend(latest.value, data.observations.at(-2).value);
+	$: previous = data.observations.at(-2);
+	$: get_trend(latest.value, previous.value);
 	let trend = {
 		direction: '',
 		difference: 0.0
@@ -12,13 +15,13 @@
 
 	function get_trend(newer, old) {
 		if (newer > old) {
-			const difference = ((newer - old) / old) * 100;
+			const difference = parseFloat((newer - old).toFixed(2));
 			trend = {
 				direction: 'up',
 				difference
 			};
 		} else if (newer < old) {
-			const difference = ((old - newer) / old) * 100;
+			const difference = parseFloat((old - newer).toFixed(2));
 			trend = {
 				direction: 'down',
 				difference
@@ -32,8 +35,45 @@
 	}
 </script>
 
-<h2 class="text-3xl font-black">
-	<span class="block text-base font-sans">Latest Rate:</span>
-	<span class="font-serif text-5xl">{latest?.value}<sup class="">%</sup></span>
-</h2>
-<p class="text-neutral-400 text-sm">As of {latest?.date}</p>
+<div class="flex flex-wrap border-y-2 border-black px-2 py-4 gap-y-6 text-center sm:text-left">
+	<div class="text-3xl font-black flex-1 flex justify-center basis-full sm:basis-0">
+		<h4>
+			<span class="block text-base font-sans mb-2">Latest Rate:</span>
+			<span class="font-serif text-4xl">{latest?.value}<sup class="">%</sup></span>
+			<span class="block text-neutral-400 text-sm font-medium">As of {latest?.date}</span>
+		</h4>
+	</div>
+	<div class="text-3xl font-black sm:border-l-2 border-black  flex-1 flex justify-center">
+		<h4>
+			<span class="block text-sm sm:text-base font-sans mb-2">Previous Rate:</span>
+			<span class="font-serif text-2xl sm:text-4xl">{previous?.value}<sup class="">%</sup></span>
+			<span class="block text-neutral-400 text-xs sm:text-sm font-medium"
+				>As of {previous?.date}</span
+			>
+		</h4>
+	</div>
+	<div
+		class="text-3xl font-black sm:border-l-2 border-black  flex-1 flex justify-center"
+		class:text-red-700={trend.direction === 'up'}
+		class:text-green-700={trend.direction === 'down'}
+	>
+		<div>
+			<p class="block text-sm sm:text-base font-sans capitalize mb-2">
+				Trending {trend.direction}:
+			</p>
+			<div class="flex gap-2">
+				{#if trend.direction === 'up'}
+					<ArrowUp size={25} />
+				{:else if trend.direction === 'down'}
+					<ArrowDown size={25} />
+				{/if}
+				<p class="font-serif text-2xl sm:text-4xl">
+					{trend?.difference}<sup class="">%</sup>
+				</p>
+			</div>
+			<p class="block text-neutral-400 text-xs sm:text-sm font-medium">
+				Since {data.observations.at(-2).date}
+			</p>
+		</div>
+	</div>
+</div>
